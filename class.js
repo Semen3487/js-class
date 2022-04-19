c58e9891c928594a56186000ac90efcb310043d1
semen <sopenday@gmail.com>
2022-04-19 16:59:24 +0300

    Add initial commit

---
+++ class.js
@@ -0,0 +1,70 @@
+// class animals {
+//   move(){
+//     console.log('I can move');
+//   }
+//   say(){
+//     console.log('I can say');
+//   }
+//   eat(){
+//     console.log('I can eat');
+//   }
+// }
+
+// class mammals extends animals{}
+
+// class birds extends animals{
+//   move(){
+//     console.log('I can fly');
+//   }
+//   say(){
+//     console.log('I can sing');
+//   }
+// }
+
+// class fish extends animals{
+//   move(){
+//     console.log('I can swim');
+//   }
+//   say(){
+//     console.log('I can not say');
+//   }
+// }
+
+// class predators extends mammals{
+//   move(){
+//     console.log('I can run');
+//   }
+// }
+
+// class whales extends mammals{}
+
+// class primates extends mammals{}
+
+// class dogs extends predators{
+//   say(){
+//     console.log('I can gav');
+//   }
+// }
+
+// class dolphin extends whales{
+//   say(){
+//     console.log('I can sing');
+//   }
+// }
+
+// class human extends primates{
+//   move(){
+//     console.log('I can go');
+//   }
+// }
+
+
+
+
+let reg = /\w{8,15}/ig;
+
+let email = /^[\w]{3,8}\.?\@\.[a-z]{2,5}$/
+ 
+  
+
+
